5e99e88aecf70f32aef6d4e762f8fa89fc17c1f9
Tim Jones <wiredforpictures@gmail.com>
2016-04-07 17:54:34 +0100

    Update specs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,101 +27,170 @@ $(function() {
     });
 
 
-    /* Loop through each feed in the allFeeds object. It should have a URL defined
-     * and the URL should not empty.
+    /* ******************* NEXT TEST ******************* */
+
+    /* Loop through each feed in the allFeeds object. It should
+     * have a URL defined and the URL should not empty.
      */
-    it('should have feeds with defined URLs which are not empty', function() {
-      allFeeds.forEach(function(feed) {
-        var url = feed.url;
+    var regularExpressionUrl = new RegExp(
+        /* Thanks to Diego Perini for the following regular
+         * expression for matching URLs.
+         * The regular expression is Copyright (c) 2010-2013 Diego Perini (http://www.iport.it).
+         */
+        "^" +
+        // protocol identifier
+        "(?:(?:https?|ftp)://)" +
+        // user:pass authentication
+        "(?:\\S+(?::\\S*)?@)?" +
+        "(?:" +
+        // IP address exclusion
+        // private & local networks
+        "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
+        "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
+        "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+        // IP address dotted notation octets
+        // excludes loopback network 0.0.0.0
+        // excludes reserved space >= 224.0.0.0
+        // excludes network & broacast addresses
+        // (first & last IP address of each class)
+        "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+        "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+        "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+        "|" +
+        // host name
+        "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" +
+        // domain name
+        "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" +
+        // TLD identifier
+        "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" +
+        // TLD may end with dot
+        "\\.?" +
+        ")" +
+        // port number
+        "(?::\\d{2,5})?" +
+        // resource path
+        "(?:[/?#]\\S*)?" +
+        "$", "i"
+    );
+
+    // Test each feed for a non-empty defined URL
+    function testEachFeedInAllFeeds(url) {
+      it('should have a feed with a defined URL which is not empty', function() {
         expect(url).toBeDefined();
-        expect(url).not.toBe('');
+        expect(url).toMatch(regularExpressionUrl);
       });
+    }
+
+    // Loop to verify each feed URL in allFeeds
+    allFeeds.forEach(function(feed) {
+      var url = feed.url;
+      testEachFeedInAllFeeds(url);
     });
 
+
+    /* ******************* NEXT TEST ******************* */
 
     /* Loop through each feed in the allFeeds object. It should have
      * a name defined and the name should not empty.
      */
-    it('should have feeds with defined names which are not empty', function() {
-      allFeeds.forEach(function(feed) {
-        var name = feed.name;
+    allFeeds.forEach(function(feed) {
+      var name = feed.name;
+      it('should have feeds with defined names which are not empty', function() {
         expect(name).toBeDefined();
         expect(name).not.toBe('');
       });
-    })
+    });
   });
 
 
-  /* TODO: Write a new test suite named "The menu" */
+  /* ******************* NEXT SUITE ******************* */
+
   describe('The menu', function() {
 
-  /* The menu element should be hidden by default. You'll
-   * have to analyze the HTML and the CSS to determine how we're
-   * performing the hiding/showing of the menu element.
-   */
+    /* ******************* NEXT TEST ******************* */
+
+    /* The menu element should be hidden by default. You'll
+     * have to analyze the HTML and the CSS to determine how we're
+     * performing the hiding/showing of the menu element.
+     */
     it('should not be displayed on load of index page', function() {
-      expect(document.body.classList.contains('menu-hidden')).toBe(true);
+      expect($('body').hasClass('menu-hidden')).toBeTruthy();
     });
 
-  /* The menu should change visibility when the menu icon is clicked.
-   * This test should have two expectations: does the menu display when
-   * clicked and does it hide when clicked again.
-   */
+
+    /* ******************* NEXT TEST ******************* */
+
+    /* The menu should change visibility when the menu icon is clicked.
+     * This test should have two expectations: does the menu display when
+     * clicked and does it hide when clicked again.
+     */
     it('should be toggled open when the menu icon is clicked', function() {
-      var menuButton = document.getElementsByClassName('menu-icon-link')[0];
+      // var menuButton = document.getElementsByClassName('menu-icon-link')[0];
+      var menuButton = $('.menu-icon-link');
       menuButton.click();
-      expect(document.body.classList.contains('menu-hidden')).toBe(false);
+      expect($('body').hasClass('menu-hidden')).toBeFalsy();
       menuButton.click();
-      expect(document.body.classList.contains('menu-hidden')).toBe(true);
+      expect($('body').hasClass('menu-hidden')).toBeTruthy();
     })
 
 
   });
 
+
+  /* ******************* NEXT SUITE ******************* */
+
   describe('Initial Entries', function() {
+
+    /* ******************* NEXT TEST ******************* */
 
     beforeEach(function(done) {
       loadFeed(0, done);
     });
 
-  /* When the loadFeed function is called and completes its work, there
-   * is at least a single .entry element within the .feed container.
-   * Remember, loadFeed() is asynchronous so this test wil require
-   * the use of Jasmine's beforeEach and asynchronous done() function.
-   */
-    it('should display at least one entry in the feed container after load', function(done) {
-      var feedContainer = document.getElementsByClassName('feed')[0];
-      expect(feedContainer.hasChildNodes()).toBe(true);
-      done();
+    /* When the loadFeed function is called and completes its work, there
+     * is at least a single .entry element within the .feed container.
+     * Remember, loadFeed() is asynchronous so this test wil require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
+     */
+    it('should display at least one entry in the feed container after load', function() {
+      var feedContainer = $('.feed').first();
+      expect(feedContainer.children('a').length).toBeGreaterThan(0);
     });
 
   });
+
+
+  /* ******************* NEXT SUITE ******************* */
 
   describe('New Feed Selection', function() {
 
-    var startingFirstItemText;
+    /* ******************* NEXT TEST ******************* */
 
-    function getFirstItemText() {
-      return document.querySelector('.feed > .entry-link article h2').textContent;
+    /* When a new feed is loaded by the loadFeed function
+     * that the content should actually change.
+     * Remember, loadFeed() is asynchronous.
+     */
+    var initialFeedTexts;
+
+    /* Get the text content of all diplayed articles in feed. */
+    function getFeedTexts() {
+      return $('.feed > .entry-link article h2').text();
     }
 
+    /* Load feed 0 first, then record feed texts, then load feed 1. */
     beforeEach(function(done) {
-      startingFirstItemText = getFirstItemText();
-      console.log('here');
       function reloadFeed() {
+        initialFeedTexts = getFeedTexts();
         loadFeed(1, done);
       }
+
       loadFeed(0, reloadFeed);
     });
 
-   /* When a new feed is loaded by the loadFeed function
-    * that the content should actually change.
-    * Remember, loadFeed() is asynchronous.
-    */
     it('should display new article entries after a new feed is loaded', function() {
-      var subsequentFirstItemText = getFirstItemText();
-      expect(subsequentFirstItemText).not.toBe(startingFirstItemText);
+      var subsequentFeedTexts = getFeedTexts();
+      expect(subsequentFeedTexts).not.toBe(initialFeedTexts);
     });
-
   });
-}());
+});
+
